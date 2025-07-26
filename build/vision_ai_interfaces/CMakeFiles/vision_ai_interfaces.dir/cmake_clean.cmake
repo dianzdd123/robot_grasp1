329e0060a66a8fc838d77f0b64d08720e0fc21c0
QC2002 <carriec9527@gmail.com>
2025-07-26 17:33:12 +0100

    Clean up build files and scan outputs

--- build/vision_ai_interfaces/CMakeFiles/vision_ai_interfaces.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/vision_ai_interfaces"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/vision_ai_interfaces.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- install/vision_ai_interfaces/lib/python3.10/site-packages/vision_ai_interfaces/srv/__init__.py
+++
@@ -1,6 +0,0 @@
-from vision_ai_interfaces.srv._execute_scan import ExecuteScan  # noqa: F401
-from vision_ai_interfaces.srv._plan_scan import PlanScan  # noqa: F401
-from vision_ai_interfaces.srv._process_detection import ProcessDetection  # noqa: F401
-from vision_ai_interfaces.srv._process_stitching import ProcessStitching  # noqa: F401
-from vision_ai_interfaces.srv._set_gripper_close import SetGripperClose  # noqa: F401
-from vision_ai_interfaces.srv._set_gripper_position import SetGripperPosition  # noqa: F401
